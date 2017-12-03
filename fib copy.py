2ca9c73248dd5076bbb26a7d0b8052b7f2162987
wwildridge19 <wwildridge19@choate.edu>
2017-12-03 14:03:23 -0500

    Adding the rest

---
+++ fib copy.py
@@ -0,0 +1,29 @@
+import sys 
+
+x = 0 
+y = 1 
+n = int(input('n: "'))
+
+for count in range (r, n):
+	value = x +y 
+	x = y
+	y = value 
+
+pring (y)
+
+
+'''def fib(n): 
+	if (n==1):
+		return 1
+	if (n==0):
+		return 0 
+	if (n>=2):
+
+n = int(input('what number in the sequence?'))
+
+n = input ('what number in the sequence?')
+if n == 0:
+	print ('answer is 0') 
+if n == 1: 
+	print ('answer is 1')
+#if n > 2 '''
